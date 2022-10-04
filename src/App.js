eac464dc307e1a21d56e00139391416835130b96
d4t06 <huudat01234560@gmail.com>
2022-10-04 11:02:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import {Routes, Route, Link} from 'react-router-dom'
-import './App.css';
-import Home from '~/pages/Home'
-import News from '~/pages/News'
-import Contact from '~/pages/Contact'
-
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.scss";
+import Home from "~/pages/Home/Home.js";
+import News from "~/pages/News";
+import Contact from "~/pages/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/news'>News</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    return (
+        <div className="App">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
 
-      <Routes>
-        <Route path='/' element={<Home></Home>}/>
-        <Route path='/News' element={<News></News>}/>
-        <Route path='/Contact' element={<Contact></Contact>}/>
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path="/" element={<Home disable></Home>} />
+                <Route path="/News" element={<News></News>} />
+                <Route path="/Contact" element={<Contact></Contact>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-
-
-
-function Home () {
-    return <h1>Đây là trang chủ</h1>
-}
-
-
-export default Home
